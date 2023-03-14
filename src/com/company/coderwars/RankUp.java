ab72237640d1acd6b29770b7ea0af97e91bfed40
Ikechukwu Paul Anene <ikpaulanene@gmail.com>
2023-03-14 13:53:33 +0100

    Implemented longestSlideDown() method for pyramid

--- src/com/company/coderwars/RankUp.java
+++ src/com/company/coderwars/RankUp.java
@@ -12,11 +12,19 @@ import static java.util.stream.IntStream.range;
 public class RankUp {
     public static void main(String[] args) {
         System.out.println("Hello from CodeWars!");
-        int[] arr = {34, 5, 8, -6, -3, -2, -1, 2, 10, 15, 16, 18, 19, 20};
-        System.out.println(rangeExtraction(arr));
+//        int[] arr = {34, 5, 8, -6, -3, -2, -1, 2, 10, 15, 16, 18, 19, 20};
+//        System.out.println(rangeExtraction(arr));
+//
+//        System.out.println(arr[0] + range(1, arr.length).mapToObj(i -> (arr[i - 1] == arr[i] - 1 ? "~" : ",") + arr[i])
+//                .collect(joining()).replaceAll("~([^,]+)~+", "-").replace("~", ","));
 
-        System.out.println(arr[0] + range(1, arr.length).mapToObj(i -> (arr[i - 1] == arr[i] - 1 ? "~" : ",") + arr[i])
-                .collect(joining()).replaceAll("~([^,]+)~+", "-").replace("~", ","));
+        int[][] pyramid = new int[][]{
+                {1},
+                {2, 3},
+                {11, 5, 6},
+                {7, 8, 9, 30}
+        };
+        System.out.println(longestSlideDown(pyramid));
     }
 
     public static String rangeExtraction(int[] arr) {
@@ -46,5 +54,14 @@ public class RankUp {
         } else {
             result.add(start + "-" + end);
         }
+    }
+
+    public static int longestSlideDown(int[][] pyramid) {
+        for (int i = pyramid.length - 2; i >= 0; i--) {
+            for (int j = 0; j < pyramid[i].length; j++) {
+                pyramid[i][j] += Math.max(pyramid[i + 1][j], pyramid[i + 1][j + 1]);
+            }
+        }
+        return pyramid[0][0];
     }
 }
--- src/com/company/coderwars/TimeFormatter.java
+++ src/com/company/coderwars/TimeFormatter.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class TimeFormatter {
 
     public static void main(String[] args) {
-        System.out.println(formatDuration(129799L));
+//        System.out.println(formatDuration(129799L));
+//        System.out.println(formatDuration(9849475));
     }
 
     public static String formatDuration(long seconds) {
@@ -24,7 +25,6 @@ public class TimeFormatter {
         timeString[4] = time[4] > 1 ? " seconds" : " second";
         StringBuilder result = new StringBuilder();
         int count = (int) Arrays.stream(time).filter(x -> x != 0).count();
-//        System.out.println("Count: " + count);
 
         for (int i = 0; i < 5; i++) {
             if (time[i] != 0) {
@@ -32,11 +32,28 @@ public class TimeFormatter {
                 result.append(time[i]).append(timeString[i]);
                 if (count == 1) {
                     result.append(" and ");
-                } else if (count < 4 && i < 4) {
+                } else if (count > 1) {
                     result.append(", ");
                 }
             }
         }
-        return result.toString();
+        return result.toString().trim();
+    }
+
+    public static String formatDuration(int t) {
+        int[] DURATIONS = {365*24*3600, 24*3600, 3600, 60, 1};
+        String[] NAMES     = {"year", "day", "hour", "minute", "second"};
+
+        if (t==0) return "now";
+
+        var sb = new StringBuilder();
+        var i  = -1;
+        for (int dt: DURATIONS) { i++;
+            var d = t/dt;
+            t %= dt;
+            if(d>0) sb.append(String.format(" %d %s%s,",d,NAMES[i],d==1?"":"s"));
+        }
+        return sb.toString().replaceAll("(^ )|(,$)","")
+                .replaceAll("(?<=.), (?=\\d+ \\w+$)", " and ");
     }
 }
